85f538b6da48d3befd54bb0c56fa7361f2916415
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-19 12:04:26 +0100

    finish

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+// creation
+let prenom;
+let age;
+let presentation;
+
+// affecter
+prenom = prompt(`Entré votre prenom`)
+age = prompt(`entré votre age`)
+presentation = `bonjour je m'appel ${prenom} et j'ai ${age}ans`
+
+// afficher
+
+alert(presentation)
